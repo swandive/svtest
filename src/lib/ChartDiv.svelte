faa64bd1994d170072f528b07e4ddd8e7a8d5980
Patrick <bzprod@gmail.com>
2021-08-18 19:27:08 -0400

    removed npm package and added unpkg cdn version to app.html. This seems to be working for now

--- src/lib/ChartDiv.svelte
+++ src/lib/ChartDiv.svelte
@@ -1,28 +1,24 @@
-<!-- <script context="module">
-    import { createChart } from 'lightweight-charts';
-    import { onMount } from 'svelte';
-  
-  
-    let chartCanvas;
-    onMount(async (promise) => {
-        const chart = createChart(chartCanvas, { width: 700, height: 500 });
-        const lineSeries = chart.addLineSeries();
-  
-        lineSeries.setData([
-            { time: '2019-04-11', value: 80.01 },
-            { time: '2019-04-12', value: 96.63 },
-            { time: '2019-04-13', value: 76.64 },
-            { time: '2019-04-14', value: 81.89 },
-            { time: '2019-04-15', value: 74.43 },
-            { time: '2019-04-16', value: 80.01 },
-            { time: '2019-04-17', value: 96.63 },
-            { time: '2019-04-18', value: 76.64 },
-            { time: '2019-04-19', value: 81.89 },
-            { time: '2019-04-20', value: 74.43 },
-        ]);
-    });
-  
-  </script>
-  
-  <div bind:this={chartCanvas} id="myChart"></div>
-   -->
+<script>
+  import { onMount } from 'svelte';
+
+  onMount(async (promise) => {
+    const chartDiv = document.getElementById('chartDiv');
+    const chart = LightweightCharts.createChart(chartDiv, { width: 640, height: 300 });
+    const lineSeries = chart.addLineSeries();
+    
+    lineSeries.setData([
+      { time: '2019-04-11', value: 80.01 },
+      { time: '2019-04-12', value: 96.63 },
+      { time: '2019-04-13', value: 76.64 },
+      { time: '2019-04-14', value: 81.89 },
+      { time: '2019-04-15', value: 74.43 },
+      { time: '2019-04-16', value: 80.01 },
+      { time: '2019-04-17', value: 96.63 },
+      { time: '2019-04-18', value: 76.64 },
+      { time: '2019-04-19', value: 81.89 },
+      { time: '2019-04-20', value: 74.43 },
+    ]);
+  })
+</script>
+
+<div class="" id="chartDiv"></div>
--- src/routes/test/index.svelte
+++ src/routes/test/index.svelte
@@ -1,28 +1,8 @@
 <script>
-  import { createChart } from 'lightweight-charts';
-  import { onMount } from 'svelte';
-
-  let chartCanvas;
-  onMount(async (promise) => {
-      const chart = createChart(chartCanvas, { width: 700, height: 500 });
-      const lineSeries = chart.addLineSeries();
-
-      lineSeries.setData([
-          { time: '2019-04-11', value: 80.01 },
-          { time: '2019-04-12', value: 96.63 },
-          { time: '2019-04-13', value: 76.64 },
-          { time: '2019-04-14', value: 81.89 },
-          { time: '2019-04-15', value: 74.43 },
-          { time: '2019-04-16', value: 80.01 },
-          { time: '2019-04-17', value: 96.63 },
-          { time: '2019-04-18', value: 76.64 },
-          { time: '2019-04-19', value: 81.89 },
-          { time: '2019-04-20', value: 74.43 },
-      ]);
-  });
-
+  import ChartDiv from '$lib/ChartDiv.svelte'
 </script>
 
-<div bind:this={chartCanvas} id="myChart"></div>
+<ChartDiv />
+<slot></slot>
 
 <a href="/">Go to Home</a>
